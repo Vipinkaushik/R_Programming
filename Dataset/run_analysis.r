f79cb4f2f99415ddab535c6578e7134e881d8090
vipkapdm <vipin_kaush@yahoo.com>
2014-12-21 11:02:53 -0500

    run analysis
    run analysis

--- Dataset/run_analysis.r
+++ Dataset/run_analysis.r
@@ -1,23 +1,23 @@
 library(plyr)
-setwd("~/GitHub/R_Programming/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset")
-trainingSubject<-read.table("~/GitHub/R_Programming/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/subject_train.txt",header=FALSE)
-trainingY<-read.table("~/GitHub/R_Programming/getdata_projectfiles_UCI HAR Dataset/train/y_train.txt",header=FALSE)
-trainingX<-read.table("~/GitHub/R_Programming/getdata_projectfiles_UCI HAR Dataset/train/X_train.txt",header=FALSE)
-testingSubject<-read.table("~/GitHub/R_Programming/getdata_projectfiles_UCI HAR Dataset/test/subject_test.txt",header=FALSE)
-testingY<-read.table("~/GitHub/R_Programming/getdata_projectfiles_UCI HAR Dataset/test/y_test.txt",header=FALSE)
-testingX<-read.table("~/GitHub/R_Programming/getdata_projectfiles_UCI HAR Dataset/X_test.txt",header=FALSE)
-features<-read.table("~/GitHub/R_Programming/getdata_projectfiles_UCI HAR Dataset/features.txt",header=FALSE)
-activityLabels<-read.table("~/GitHub/R_Programming/getdata_projectfiles_UCI HAR Dataset/activity_labels.txt",header=FALSE)
+setwd("~/GitHub/R_Programming/Dataset")
+trainingSubject<-read.table("~/GitHub/R_Programming/Dataset/train/subject_train.txt",header=FALSE)
+trainingY<-read.table("~/GitHub/R_Programming/Dataset/train/y_train.txt",header=FALSE)
+trainingX<-read.table("~/GitHub/R_Programming/Dataset/train/x_train.txt",header=FALSE)
+testingSubject<-read.table("~/GitHub/R_Programming/Dataset/test/subject_test.txt",header=FALSE)
+testingY<-read.table("~/GitHub/R_Programming/Dataset/test/y_test.txt",header=FALSE)
+testingX<-read.table("~/GitHub/R_Programming/Dataset/test/x_test.txt",header=FALSE)
+features<-read.table("~/GitHub/R_Programming/Dataset/features.txt",header=FALSE)
+activityLabels<-read.table("~/GitHub/R_Programming/Dataset/activity_labels.txt",header=FALSE)
 
 
 subjects<-rbind(trainingSubject,testingSubject)
-activities<-rbind(traningY,testingY)
-readings<-rbind(traningX,testingX)
+activities<-rbind(trainingY,testingY)
+readings<-rbind(trainingX,testingX)
 
 tmp<-count(unique(subjects)); subjectNum<-sum(tmp$freq)
 tmp<-count(unique(activityLabels)); labelNum<-sum(tmp$freq)
 
-rm(trainingSubject,testingSubject,traningY,testingY,traningX,testingX)
+
 
 activities$activityName<-activityLabels[activities$V1,2]
 
@@ -36,7 +36,7 @@ master<-cbind(master,readings[,41:46],readings[,214:215],readings[,253:254],
               readings[,529:530],readings[,542:543])
 
 
-rm(subjects,activities,readings,features,cleanFeatures)
+
 
 master<-master[order(master$activitySubject,desc(master$activityLabel)),]
 
@@ -60,12 +60,10 @@ for(i in 1:labelNum){
   }
 }
 
-write.table(master,"masterData.txt",row.names=T,col.names=T) 
-write.table(output,"activitySubjectMeans.txt",row.names=T,col.names=T) 
+write.table(master,"~/GitHub/R_Programming/Dataset/masterData.txt",row.names=T,col.names=T) 
+write.table(output,"~/GitHub/R_Programming/Dataset/activitySubjectMeans.txt",row.names=T,col.names=T) 
 listOfVariables <- data.frame(names(output))
-write.csv(listOfVariables,"listOfVariables.csv")
+write.csv(listOfVariables,"~/GitHub/R_Programming/Dataset/listOfVariables.csv")
 
+setwd("~/GitHub/R_Programming/Dataset")
 
-rm(tmp,tmpAct,tmpActSubj,activityLabels,listOfVariables)
-rm(master,output,i,j,k,labelNum,outputCtr,subjectNum)
-setwd("../")
